b9e6a69f74f00b391209bf7cd085b289b0730ec9
Paul Cummin <paulcummin7@gmail.com>
2026-02-01 17:29:12 +0000

    File changes

--- src/components/room/rv/RenderPrimitives.jsx
+++ src/components/room/rv/RenderPrimitives.jsx
@@ -200,7 +200,7 @@ export const SpeakerIcon = React.memo(function SpeakerIcon({
       <path
         d={pathData}
         fill={color || "#000000"}
-        stroke="#000000"}
+        stroke="#000000"
         strokeWidth={1}
         opacity={1}
         onMouseEnter={(e) => {
