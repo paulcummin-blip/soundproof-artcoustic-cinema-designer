0c748a4b0abc8b618f833ec3905c455c3f924436
Paul Cummin <paulcummin7@gmail.com>
2026-03-04 18:54:38 +0000

    File changes

---
+++ src/components/room/utils/overheadZones.jsx
@@ -0,0 +1,5 @@
+/**
+ * components/room/utils/overheadZones.js
+ * Re-exports computeOverheadZones from the canonical location.
+ */
+export { computeOverheadZones } from '@/components/utils/overheadZones';
