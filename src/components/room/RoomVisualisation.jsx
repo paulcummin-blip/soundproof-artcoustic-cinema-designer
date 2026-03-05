47e625081f9e8d60c31261599a1bb653c894a083
Paul Cummin <paulcummin7@gmail.com>
2026-03-05 13:16:26 +0000

    File changes

--- src/components/room/RoomVisualisation.jsx
+++ src/components/room/RoomVisualisation.jsx
@@ -41,7 +41,7 @@ import { useRoomCoordinateConverters } from "@/components/room/rv/hooks/useRoomC
 import { useFrontWideZonesComputed } from "@/components/room/rv/hooks/useFrontWideZonesComputed";
 import { useOverheadZonesComputed } from "@/components/room/rv/hooks/useOverheadZonesComputed";
 import { usePanZoomHandlers } from "@/components/room/rv/hooks/usePanZoomHandlers";
-import { useZoneComponents, useRenderFrontWideZones } from "@/components/room/rv/hooks/useZoneComponents";
+import { useZoneComponents } from "@/components/room/rv/hooks/useZoneComponents"; import { useRenderFrontWideZones } from "@/components/room/rv/hooks/useRenderFrontWideZones";
 
 
 // --- OVERHEAD HELPERS (RoomVisualisation) ---
