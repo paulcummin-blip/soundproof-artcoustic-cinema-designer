5da9732bdff31464af0590c8f373ba5c529717fc
Paul Cummin <paulcummin7@gmail.com>
2026-03-07 22:31:58 +0000

    File changes

--- src/components/projects/NewProjectDialog.jsx
+++ src/components/projects/NewProjectDialog.jsx
@@ -217,7 +217,7 @@ export default function NewProjectDialog({ open, onOpenChange, onProjectCreated 
             </Button>
             <Button 
               type="submit" 
-              className="bg-[#1B1A1A] hover:bg-[#3E4349] text-[#3E4349]"
+              className="bg-[#1B1A1A] hover:bg-[#3E4349] text-white"
               disabled={!formData.name}
             >
               Create Project
