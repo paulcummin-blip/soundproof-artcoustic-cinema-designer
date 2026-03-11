5ceec28875e6626e540d3bb8a91eabd0fb044e88
Paul Cummin <paulcummin7@gmail.com>
2026-03-11 23:58:20 +0000

    File changes

--- src/components/roomdesigner/useGuardedSetter.jsx
+++ src/components/roomdesigner/useGuardedSetter.jsx
@@ -3,7 +3,7 @@ import { useAppState } from "@/components/AppStateProvider";
 
 export function useGuardedSetter(setter, tabName) {
   const { isFrozen } = useAppState();
-  return React.useCallback((next) => {
+  return useCallback((next) => {
     if (isFrozen?.(tabName)) return;
     setter?.(next);
   }, [setter, isFrozen, tabName]);
