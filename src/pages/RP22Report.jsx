ad9dbfc508ef6cf2e63edf3005498051a50afcfc
Paul Cummin <paulcummin7@gmail.com>
2026-03-16 22:02:49 +0000

    File changes

--- src/pages/RP22Report.jsx
+++ src/pages/RP22Report.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAppState } from '../components/AppStateProvider';
 // TEMP DEBUG: remove after sub persistence proven
-import { useActiveProjectId, fetchProjectById } from '@/components/state/project-session';
+import { useActiveProjectId } from '@/components/state/project-session';
 // END TEMP DEBUG
 import { useRP22AnalysisEngine } from '../components/hooks/useRP22AnalysisEngine';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
