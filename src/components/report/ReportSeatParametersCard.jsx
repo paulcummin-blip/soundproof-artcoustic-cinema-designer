a0137b07bfac4a12953e645b79ca32ce8702250d
Paul Cummin <paulcummin7@gmail.com>
2026-03-17 12:47:23 +0000

    File changes

--- src/components/report/ReportSeatParametersCard.jsx
+++ src/components/report/ReportSeatParametersCard.jsx
@@ -11,7 +11,6 @@ export default function ReportSeatParametersCard({
     app,
     rspSeatId,
     analysisResult,
-    headerSlot,
 }) {
     return (
         <Card className="bg-[#FFFFFF] border-[#DCDBD6] mt-6">
@@ -22,11 +21,6 @@ export default function ReportSeatParametersCard({
             </CardHeader>
             <CardContent>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
-                    {headerSlot && (
-                        <div className="col-span-1 md:col-span-2 lg:col-span-2">
-                            {headerSlot}
-                        </div>
-                    )}
                     {!hasSeats ? (
                         <p className="text-sm text-[#3E4349]">
                             No seats defined yet. Configure seating in Room Designer.
