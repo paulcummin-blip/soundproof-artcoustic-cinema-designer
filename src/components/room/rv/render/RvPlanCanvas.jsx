98bfcbf33396ef59973011049772b4aa7d6bfbb7
Paul Cummin <paulcummin7@gmail.com>
2026-03-17 17:42:21 +0000

    File changes

--- src/components/room/rv/render/RvPlanCanvas.jsx
+++ src/components/room/rv/render/RvPlanCanvas.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { hasPos } from "@/components/room/rv/RenderPrimitives";
 import RvSpeakerLayer from "@/components/room/rv/render/RvSpeakerLayer";
 import SvgDefs from "@/components/room/SvgDefs";
